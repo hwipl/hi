6dcabb0a28a65e30ed11cafa845396cd381a3013
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-14 21:26:56 +0200

    add mdns
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
-fn main() {
-    println!("Hello, world!");
+use futures::executor::block_on;
+use futures::prelude::*;
+use libp2p::mdns::{Mdns, MdnsConfig, MdnsEvent};
+use libp2p::swarm::{NetworkBehaviourEventProcess, Swarm};
+use libp2p::{identity, NetworkBehaviour, PeerId};
+use std::error::Error;
+use std::task::Poll;
+
+/// Custom network behaviour with mdns
+#[derive(NetworkBehaviour)]
+struct HiBehaviour {
+    mdns: Mdns,
+}
+
+impl NetworkBehaviourEventProcess<MdnsEvent> for HiBehaviour {
+    // handle `mdns` events
+    fn inject_event(&mut self, event: MdnsEvent) {
+        match event {
+            MdnsEvent::Discovered(list) => {
+                for (peer, addr) in list {
+                    println!("Peer discovered: {:?} {:?}", peer, addr);
+                }
+            }
+            MdnsEvent::Expired(list) => {
+                for (peer, addr) in list {
+                    if !self.mdns.has_node(&peer) {
+                        println!("Peer expired: {:?} {:?}", peer, addr);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // create key and peer id
+    let local_key = identity::Keypair::generate_ed25519();
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {:?}", local_peer_id);
+
+    // create transport
+    let transport = block_on(libp2p::development_transport(local_key))?;
+
+    // create mdns
+    let mdns = block_on(Mdns::new(MdnsConfig::default()))?;
+
+    // create network behaviour
+    let behaviour = HiBehaviour { mdns };
+
+    // create swarm
+    let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
+
+    // listen on all IPs and random ports.
+    swarm.listen_on("/ip6/::/tcp/0".parse()?)?;
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    // start main loop
+    block_on(future::poll_fn(move |cx| loop {
+        match swarm.poll_next_unpin(cx) {
+            Poll::Ready(Some(event)) => println!("event: {:?}", event),
+            Poll::Ready(None) => return Poll::Ready(()),
+            Poll::Pending => return Poll::Pending,
+        }
+    }));
+
+    Ok(())
 }
