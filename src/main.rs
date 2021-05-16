49775139396e600a0064efcc7ff4c202080c7a59
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-16 13:52:41 +0200

    add gossipsub
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use futures::executor::block_on;
 use futures::prelude::*;
+use libp2p::gossipsub::{
+    Gossipsub, GossipsubConfig, GossipsubEvent, IdentTopic, MessageAuthenticity,
+};
 use libp2p::mdns::{Mdns, MdnsConfig, MdnsEvent};
 use libp2p::swarm::{NetworkBehaviourEventProcess, Swarm};
 use libp2p::{identity, NetworkBehaviour, PeerId};
@@ -9,7 +12,28 @@ use std::task::Poll;
 /// Custom network behaviour with mdns
 #[derive(NetworkBehaviour)]
 struct HiBehaviour {
+    gossip: Gossipsub,
     mdns: Mdns,
+}
+
+impl NetworkBehaviourEventProcess<GossipsubEvent> for HiBehaviour {
+    // handle `gossip` events
+    fn inject_event(&mut self, event: GossipsubEvent) {
+        match event {
+            GossipsubEvent::Message { message, .. } => {
+                println!(
+                    "Message: {:?} -> {:?}: {:?}",
+                    message.source, message.topic, message.data,
+                );
+            }
+            GossipsubEvent::Subscribed { peer_id, topic } => {
+                println!("Subscribed: {:?} {:?}", peer_id, topic);
+            }
+            GossipsubEvent::Unsubscribed { peer_id, topic } => {
+                println!("Unsubscribed: {:?} {:?}", peer_id, topic);
+            }
+        }
+    }
 }
 
 impl NetworkBehaviourEventProcess<MdnsEvent> for HiBehaviour {
@@ -39,13 +63,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("Local peer id: {:?}", local_peer_id);
 
     // create transport
-    let transport = block_on(libp2p::development_transport(local_key))?;
+    let transport = block_on(libp2p::development_transport(local_key.clone()))?;
 
     // create mdns
     let mdns = block_on(Mdns::new(MdnsConfig::default()))?;
 
+    // create gossip
+    let message_authenticity = MessageAuthenticity::Signed(local_key);
+    let gossipsub_config = GossipsubConfig::default();
+    let mut gossip: Gossipsub = Gossipsub::new(message_authenticity, gossipsub_config)?;
+
+    // subscribe to topic
+    let topic = IdentTopic::new("/hello/world");
+    gossip.subscribe(&topic).unwrap();
+
     // create network behaviour
-    let behaviour = HiBehaviour { mdns };
+    let behaviour = HiBehaviour { gossip, mdns };
 
     // create swarm
     let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
