5da9c7668ab810f84c9811086d2790aa705fee6c
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-19 15:00:31 +0200

    add request-response
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,155 @@
+use async_trait::async_trait;
 use futures::{executor::block_on, prelude::*, select};
+use libp2p::core::{
+    upgrade::{read_one, write_one},
+    ProtocolName,
+};
 use libp2p::gossipsub::{
     Gossipsub, GossipsubConfig, GossipsubEvent, IdentTopic, MessageAuthenticity,
 };
 use libp2p::mdns::{Mdns, MdnsConfig, MdnsEvent};
+use libp2p::request_response::{
+    ProtocolSupport, RequestResponse, RequestResponseCodec, RequestResponseConfig,
+    RequestResponseEvent, RequestResponseMessage,
+};
 use libp2p::swarm::{NetworkBehaviourEventProcess, Swarm, SwarmEvent};
 use libp2p::{identity, Multiaddr, NetworkBehaviour, PeerId};
 use minicbor::{Decode, Encode};
 use std::error::Error;
 use std::time::Duration;
+use std::{io, iter};
 use wasm_timer::Delay;
 
-/// Custom network behaviour with mdns and gossipsub
+/// Request-response protocol for the request-response behaviour
+#[derive(Debug, Clone)]
+struct HiRequestProtocol();
+
+impl ProtocolName for HiRequestProtocol {
+    fn protocol_name(&self) -> &[u8] {
+        "/hi/request/0.0.1".as_bytes()
+    }
+}
+
+/// Codec for the request-response behaviour
+#[derive(Clone)]
+struct HiCodec();
+
+#[async_trait]
+impl RequestResponseCodec for HiCodec {
+    type Protocol = HiRequestProtocol;
+    type Request = HiRequest;
+    type Response = HiResponse;
+
+    async fn read_request<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+    ) -> io::Result<Self::Request>
+    where
+        T: AsyncRead + Unpin + Send,
+    {
+        read_one(io, 1024)
+            .map(|res| match res {
+                Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
+                Ok(vec) => Ok(HiRequest(vec)),
+            })
+            .await
+    }
+
+    async fn read_response<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+    ) -> io::Result<Self::Response>
+    where
+        T: AsyncRead + Unpin + Send,
+    {
+        read_one(io, 1024)
+            .map(|res| match res {
+                Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
+                Ok(vec) => Ok(HiResponse(vec)),
+            })
+            .await
+    }
+
+    async fn write_request<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+        HiRequest(data): HiRequest,
+    ) -> io::Result<()>
+    where
+        T: AsyncWrite + Unpin + Send,
+    {
+        write_one(io, data).await
+    }
+
+    async fn write_response<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+        HiResponse(data): HiResponse,
+    ) -> io::Result<()>
+    where
+        T: AsyncWrite + Unpin + Send,
+    {
+        write_one(io, data).await
+    }
+}
+
+/// Request message
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct HiRequest(Vec<u8>);
+
+/// Response message
+#[derive(Debug, Clone, PartialEq, Eq)]
+struct HiResponse(Vec<u8>);
+
+/// Custom network behaviour with mdns, gossipsub, request-response
 #[derive(NetworkBehaviour)]
 struct HiBehaviour {
+    request: RequestResponse<HiCodec>,
     gossip: Gossipsub,
     mdns: Mdns,
+}
+
+impl NetworkBehaviourEventProcess<RequestResponseEvent<HiRequest, HiResponse>> for HiBehaviour {
+    // hande `request` events
+    fn inject_event(&mut self, message: RequestResponseEvent<HiRequest, HiResponse>) {
+        // create messages
+        let request = HiRequest("hey".to_string().into_bytes());
+        let response = HiResponse("hi".to_string().into_bytes());
+
+        // handle incoming messages
+        if let RequestResponseEvent::Message { peer, message } = message {
+            match message {
+                // handle incoming request message, send back response
+                RequestResponseMessage::Request { channel, .. } => {
+                    println!("received request {:?} from {:?}", request, peer);
+                    self.request
+                        .send_response(channel, response.clone())
+                        .unwrap();
+                    return;
+                }
+
+                // handle incoming response message
+                RequestResponseMessage::Response { response, .. } => {
+                    println!("received response {:?} from {:?}", response, peer);
+                    return;
+                }
+            }
+        }
+
+        // handle response sent event
+        if let RequestResponseEvent::ResponseSent { peer, .. } = message {
+            println!("sent response {:?} to {:?}", response, peer);
+            return;
+        }
+
+        println!("request response error: {:?}", message);
+    }
 }
 
 impl NetworkBehaviourEventProcess<GossipsubEvent> for HiBehaviour {
@@ -181,8 +316,17 @@ fn main() -> Result<(), Box<dyn Error>> {
     let topic = IdentTopic::new("/hello/world");
     gossip.subscribe(&topic).unwrap();
 
+    // create request-response
+    let protocols = iter::once((HiRequestProtocol(), ProtocolSupport::Full));
+    let cfg = RequestResponseConfig::default();
+    let request = RequestResponse::new(HiCodec(), protocols.clone(), cfg.clone());
+
     // create network behaviour
-    let behaviour = HiBehaviour { gossip, mdns };
+    let behaviour = HiBehaviour {
+        request,
+        gossip,
+        mdns,
+    };
 
     // create swarm
     let mut swarm = Swarm::new(transport, behaviour, local_peer_id);
