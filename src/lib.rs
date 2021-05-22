88afe044067ccc3bd2dc29a0e06a9e47dfd578bf
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-22 16:52:23 +0200

    move request-response code to request.rs
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,116 +1,27 @@
-use async_trait::async_trait;
+mod request;
+
 use futures::{executor::block_on, prelude::*, select};
-use libp2p::core::{
-    upgrade::{read_one, write_one},
-    ProtocolName,
-};
 use libp2p::gossipsub::{
     Gossipsub, GossipsubConfig, GossipsubEvent, IdentTopic, MessageAuthenticity,
 };
 use libp2p::mdns::{Mdns, MdnsConfig, MdnsEvent};
 use libp2p::request_response::{
-    ProtocolSupport, RequestResponse, RequestResponseCodec, RequestResponseConfig,
-    RequestResponseEvent, RequestResponseMessage,
+    ProtocolSupport, RequestResponse, RequestResponseConfig, RequestResponseEvent,
+    RequestResponseMessage,
 };
 use libp2p::swarm::{NetworkBehaviourEventProcess, Swarm, SwarmEvent};
 use libp2p::{identity, Multiaddr, NetworkBehaviour, PeerId};
 use minicbor::{Decode, Encode};
+use request::{HiCodec, HiRequest, HiRequestProtocol, HiResponse};
 use std::error::Error;
+use std::iter;
 use std::time::Duration;
-use std::{io, iter};
 use wasm_timer::Delay;
-
-/// Request-response protocol for the request-response behaviour
-#[derive(Debug, Clone)]
-struct HiRequestProtocol();
-
-impl ProtocolName for HiRequestProtocol {
-    fn protocol_name(&self) -> &[u8] {
-        "/hi/request/0.0.1".as_bytes()
-    }
-}
-
-/// Codec for the request-response behaviour
-#[derive(Clone)]
-struct HiCodec();
-
-#[async_trait]
-impl RequestResponseCodec for HiCodec {
-    type Protocol = HiRequestProtocol;
-    type Request = HiRequest;
-    type Response = HiResponse;
-
-    async fn read_request<T>(
-        &mut self,
-        _: &HiRequestProtocol,
-        io: &mut T,
-    ) -> io::Result<Self::Request>
-    where
-        T: AsyncRead + Unpin + Send,
-    {
-        read_one(io, 1024)
-            .map(|res| match res {
-                Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
-                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
-                Ok(vec) => Ok(HiRequest(vec)),
-            })
-            .await
-    }
-
-    async fn read_response<T>(
-        &mut self,
-        _: &HiRequestProtocol,
-        io: &mut T,
-    ) -> io::Result<Self::Response>
-    where
-        T: AsyncRead + Unpin + Send,
-    {
-        read_one(io, 1024)
-            .map(|res| match res {
-                Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
-                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
-                Ok(vec) => Ok(HiResponse(vec)),
-            })
-            .await
-    }
-
-    async fn write_request<T>(
-        &mut self,
-        _: &HiRequestProtocol,
-        io: &mut T,
-        HiRequest(data): HiRequest,
-    ) -> io::Result<()>
-    where
-        T: AsyncWrite + Unpin + Send,
-    {
-        write_one(io, data).await
-    }
-
-    async fn write_response<T>(
-        &mut self,
-        _: &HiRequestProtocol,
-        io: &mut T,
-        HiResponse(data): HiResponse,
-    ) -> io::Result<()>
-    where
-        T: AsyncWrite + Unpin + Send,
-    {
-        write_one(io, data).await
-    }
-}
-
-/// Request message
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct HiRequest(Vec<u8>);
-
-/// Response message
-#[derive(Debug, Clone, PartialEq, Eq)]
-struct HiResponse(Vec<u8>);
 
 /// Custom network behaviour with mdns, gossipsub, request-response
 #[derive(NetworkBehaviour)]
 struct HiBehaviour {
-    request: RequestResponse<HiCodec>,
+    request: RequestResponse<request::HiCodec>,
     gossip: Gossipsub,
     mdns: Mdns,
 }
---
+++ src/request.rs
@@ -0,0 +1,96 @@
+use async_trait::async_trait;
+
+use futures::prelude::*;
+use libp2p::core::{
+    upgrade::{read_one, write_one},
+    ProtocolName,
+};
+use libp2p::request_response::RequestResponseCodec;
+use std::io;
+
+/// Request-response protocol for the request-response behaviour
+#[derive(Debug, Clone)]
+pub struct HiRequestProtocol();
+
+impl ProtocolName for HiRequestProtocol {
+    fn protocol_name(&self) -> &[u8] {
+        "/hi/request/0.0.1".as_bytes()
+    }
+}
+
+/// Codec for the request-response behaviour
+#[derive(Clone)]
+pub struct HiCodec();
+
+#[async_trait]
+impl RequestResponseCodec for HiCodec {
+    type Protocol = HiRequestProtocol;
+    type Request = HiRequest;
+    type Response = HiResponse;
+
+    async fn read_request<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+    ) -> io::Result<Self::Request>
+    where
+        T: AsyncRead + Unpin + Send,
+    {
+        read_one(io, 1024)
+            .map(|res| match res {
+                Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
+                Ok(vec) => Ok(HiRequest(vec)),
+            })
+            .await
+    }
+
+    async fn read_response<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+    ) -> io::Result<Self::Response>
+    where
+        T: AsyncRead + Unpin + Send,
+    {
+        read_one(io, 1024)
+            .map(|res| match res {
+                Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
+                Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
+                Ok(vec) => Ok(HiResponse(vec)),
+            })
+            .await
+    }
+
+    async fn write_request<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+        HiRequest(data): HiRequest,
+    ) -> io::Result<()>
+    where
+        T: AsyncWrite + Unpin + Send,
+    {
+        write_one(io, data).await
+    }
+
+    async fn write_response<T>(
+        &mut self,
+        _: &HiRequestProtocol,
+        io: &mut T,
+        HiResponse(data): HiResponse,
+    ) -> io::Result<()>
+    where
+        T: AsyncWrite + Unpin + Send,
+    {
+        write_one(io, data).await
+    }
+}
+
+/// Request message
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HiRequest(pub Vec<u8>);
+
+/// Response message
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct HiResponse(pub Vec<u8>);
