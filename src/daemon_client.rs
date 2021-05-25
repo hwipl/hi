4f2ff6497defe37739970e844cf1f37be46ff291
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-25 15:40:24 +0200

    daemon_client: send connect requests in config to server
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/daemon_client.rs
+++ src/daemon_client.rs
@@ -1,8 +1,28 @@
 use crate::config;
+use crate::daemon_message::Message;
 use crate::unix_socket;
 use async_std::task;
 
-pub fn run(_: config::Config) {
+/// run daemon client with config
+async fn run_client(config: config::Config, mut client: unix_socket::UnixClient) {
+    // handle connect addresses in config
+    for addr in config.connect {
+        // send connect request
+        let msg = Message::ConnectAddress { addr };
+        if let Err(e) = client.send_message(msg).await {
+            eprintln!("error sending connect message: {}", e);
+        }
+
+        // receive reply
+        match client.receive_message().await {
+            Ok(msg) => println!("reply from server: {:?}", msg),
+            Err(e) => eprintln!("error receiving reply: {}", e),
+        }
+    }
+}
+
+/// initialize client connection and run daemon client
+pub fn run(config: config::Config) {
     // run unix socket client
     task::block_on(async {
         match unix_socket::UnixClient::connect().await {
@@ -10,6 +30,7 @@ pub fn run(_: config::Config) {
                 if let Err(e) = client.test().await {
                     eprintln!("unix socket test error: {}", e);
                 }
+                run_client(config, client).await;
             }
             Err(e) => {
                 eprintln!("unix socket client error: {}", e);
