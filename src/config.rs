de9661e28b267faa7bc1f07ce211a82fd3a150a5
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-26 17:46:22 +0200

    config: add struct ConfigOption
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,35 @@
 use clap::{AppSettings, Clap};
+use std::str::FromStr;
+
+/// Configuration option for setting and getting:
+/// setting requires name and value, getting only requires name
+pub struct ConfigOption {
+    pub name: String,
+    pub value: String,
+}
+
+impl FromStr for ConfigOption {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        // parse option string and get name and value
+        let parts: Vec<&str> = s.split(":").collect();
+        if parts.len() < 1 || parts.len() > 2 {
+            return Err(String::from("invalid configuration option"));
+        }
+        let name = String::from(parts[0]);
+
+        // only name given?
+        if parts.len() == 1 {
+            let value = String::from("");
+            return Ok(ConfigOption { name, value });
+        }
+
+        // name and value given
+        let value = String::from(parts[1]);
+        Ok(ConfigOption { name, value })
+    }
+}
 
 #[derive(Clap)]
 #[clap(version)]
@@ -11,6 +42,14 @@ pub struct Config {
     /// Connect to peer addresses.
     #[clap(short, long, name = "address")]
     pub connect: Vec<String>,
+
+    /// Set configuration options
+    #[clap(long, name = "option:value")]
+    pub set: Vec<ConfigOption>,
+
+    /// Get configuration options
+    #[clap(long, name = "option")]
+    pub get: Vec<ConfigOption>,
 }
 
 /// get config
