f4aff27165caa30cd7d5b904bd83e102379abdfd
hwipl <33433250+hwipl@users.noreply.github.com>
2021-05-30 20:14:38 +0200

    daemon_client: add chat mode
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/daemon_client.rs
+++ src/daemon_client.rs
@@ -1,7 +1,50 @@
 use crate::config;
 use crate::daemon_message::Message;
 use crate::unix_socket;
-use async_std::task;
+use async_std::{io, prelude::*, task};
+
+/// run daemon client in chat mode
+async fn run_chat_client(mut client: unix_socket::UnixClient, destination: String) {
+    // enable chat mode for this client
+    let msg = Message::SetChat { enabled: true };
+    if let Err(e) = client.send_message(msg).await {
+        eprintln!("error sending set chat message: {}", e);
+        return;
+    }
+
+    // receive reply
+    match client.receive_message().await {
+        Ok(msg) => {
+            println!("set chat reply from server: {:?}", msg);
+            match msg {
+                Message::Ok => (),
+                _ => {
+                    eprintln!("unexpected reply from server");
+                    return;
+                }
+            }
+        }
+        Err(e) => {
+            eprintln!("error receiving set chat reply: {}", e);
+            return;
+        }
+    }
+
+    // enter chat mode
+    println!("Chat mode:");
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+    while let Some(Ok(line)) = stdin.next().await {
+        let msg = Message::ChatMessage {
+            to: destination.clone(),
+            from: String::new(),
+            message: line,
+        };
+        if let Err(e) = client.send_message(msg).await {
+            eprintln!("error sending chat message: {}", e);
+            return;
+        }
+    }
+}
 
 /// run daemon client with config
 async fn run_client(config: config::Config, mut client: unix_socket::UnixClient) {
@@ -21,10 +64,16 @@ async fn run_client(config: config::Config, mut client: unix_socket::UnixClient)
     }
 
     // handle set configuration options in config
+    let mut chat_partner = String::new();
     for option in config.set {
         // create message
         let msg = match option.name.as_str() {
             "name" => Message::SetName { name: option.value },
+            "chat" => {
+                // set chat partner for chat mode below
+                chat_partner = option.value;
+                continue;
+            }
             _ => {
                 eprintln!(
                     "error setting unknown configuration option: {}",
@@ -72,6 +121,12 @@ async fn run_client(config: config::Config, mut client: unix_socket::UnixClient)
             Ok(msg) => println!("get reply from server: {:?}", msg),
             Err(e) => eprintln!("error receiving get reply: {}", e),
         }
+    }
+
+    // handle chat mode
+    if chat_partner != "" {
+        run_chat_client(client, chat_partner).await;
+        return;
     }
 }
 
