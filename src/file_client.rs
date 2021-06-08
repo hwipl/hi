99c006d77f41bcf4840d501964e655d1ca2c093e
hwipl <33433250+hwipl@users.noreply.github.com>
2021-06-08 20:19:00 +0200

    file_client: add reading lines from stdin
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/file_client.rs
+++ src/file_client.rs
@@ -1,5 +1,8 @@
 use crate::daemon_message::{FileInfo, Message};
 use crate::unix_socket;
+use async_std::{io, prelude::*};
+use futures::future::FutureExt;
+use futures::select;
 
 /// run daemon client in file mode
 pub async fn run_file_client(
@@ -74,15 +77,38 @@ pub async fn run_file_client(
     }
 
     // enter file loop
-    println!("Files:");
-    while let Ok(msg) = client.receive_message().await {
-        match msg {
-            Message::GetFiles { files } => {
-                for f in files {
-                    println!("{}: {} ({})", f.peer_id, f.name, f.size);
+    println!("File mode:");
+    let mut stdin = io::BufReader::new(io::stdin()).lines();
+    loop {
+        select! {
+            // handle message coming from daemon
+            msg = client.receive_message().fuse() => {
+                match msg {
+                    Ok(Message::GetFiles { files }) => {
+                        for f in files {
+                            println!("{}: {} ({})", f.peer_id, f.name, f.size);
+                        }
+                    }
+                    _ => (),
                 }
-            }
-            _ => (),
+            },
+
+            // handle line read from stdin
+            line = stdin.next().fuse() => {
+                let line = match line {
+                    Some(Ok(line)) if line != "" => line,
+                    _ => continue,
+                };
+                let msg = Message::FileMessage {
+                    to: String::from("all"),
+                    from: String::new(),
+                    content: line.into_bytes(),
+                };
+                if let Err(e) = client.send_message(msg).await {
+                    eprintln!("error sending file message: {}", e);
+                    return;
+                }
+            },
         }
     }
 }
