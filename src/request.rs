b48606da9586fe0a126f4ad072b2721dee584039
hwipl <33433250+hwipl@users.noreply.github.com>
2021-06-10 11:02:28 +0200

    request: remove DownloadFile response
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/request.rs
+++ src/request.rs
@@ -1,9 +1,8 @@
-use async_std::fs::{self, File};
 use async_std::io;
 use async_trait::async_trait;
 use futures::prelude::*;
 use libp2p::core::{
-    upgrade::{read_one, write_one, write_with_len_prefix},
+    upgrade::{read_one, write_one},
     ProtocolName,
 };
 use libp2p::request_response::RequestResponseCodec;
@@ -56,7 +55,7 @@ impl RequestResponseCodec for HiCodec {
     where
         T: AsyncRead + Unpin + Send,
     {
-        let response = read_one(io, 1024)
+        read_one(io, 1024)
             .map(|res| match res {
                 Err(e) => Err(io::Error::new(io::ErrorKind::InvalidData, e)),
                 Ok(vec) if vec.is_empty() => Err(io::ErrorKind::UnexpectedEof.into()),
@@ -64,16 +63,7 @@ impl RequestResponseCodec for HiCodec {
                     minicbor::decode(&vec).map_err(|e| io::Error::new(io::ErrorKind::Other, e))
                 }
             })
-            .await;
-
-        match &response {
-            Ok(HiResponse::DownloadFile(file, size)) => {
-                println!("got download file response: {} ({})", file, size);
-                download_file(file.clone(), *size, io).await;
-            }
-            _ => (),
-        }
-        response
+            .await
     }
 
     async fn write_request<T>(
@@ -102,15 +92,6 @@ impl RequestResponseCodec for HiCodec {
     where
         T: AsyncWrite + Unpin + Send,
     {
-        // special response handling
-        match response {
-            HiResponse::DownloadFile(file, ..) => {
-                return upload_file(file, io).await;
-            }
-            _ => (),
-        }
-
-        // send message
         let mut buffer = Vec::new();
         if let Err(e) = minicbor::encode(response, &mut buffer) {
             eprintln!("error encoding response message: {}", e);
@@ -144,46 +125,4 @@ pub enum HiResponse {
     Data(#[n(0)] Vec<u8>),
     #[n(3)]
     FileList(#[n(0)] Vec<(String, u64)>),
-    #[n(4)]
-    DownloadFile(#[n(0)] String, #[n(1)] u64),
-}
-
-/// download file coming from other peer
-async fn download_file<T>(file: String, size: u64, io: &mut T)
-where
-    T: AsyncRead + Unpin + Send,
-{
-    if let Ok(mut out) = File::create(format!("temp-{}", file)).await {
-        if let Err(e) = io::copy(&mut io.take(size), &mut out).await {
-            eprintln!("error downloading file {}: {}", file, e);
-        }
-    }
-}
-
-/// upload file to other peer
-async fn upload_file<T>(file: String, io: &mut T) -> io::Result<()>
-where
-    T: AsyncWrite + Unpin + Send,
-{
-    // check actual file size
-    let size = fs::metadata(&file).await?.len();
-
-    // send response with actual file size
-    let response = HiResponse::DownloadFile(file.clone(), size);
-    let mut buffer = Vec::new();
-    if let Err(e) = minicbor::encode(response, &mut buffer) {
-        eprintln!("error encoding response message: {}", e);
-        return Err(io::Error::new(io::ErrorKind::Other, e));
-    }
-    write_with_len_prefix(io, buffer).await?;
-
-    // upload file
-    let mut input = File::open(file.clone()).await?;
-    if let Err(e) = io::copy(&mut input, io).await {
-        eprintln!("error uploading file {}: {}", file, e);
-        return Err(io::Error::new(io::ErrorKind::Other, e));
-    }
-
-    // close connection
-    io.close().await
 }
