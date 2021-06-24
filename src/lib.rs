10a0d67ea61af1332ce32b875ad6f2e8baa84086
hwipl <33433250+hwipl@users.noreply.github.com>
2021-06-24 14:51:20 +0200

    move chat_client to clients/chat
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@
 extern crate log;
 
 mod behaviour;
-mod chat_client;
 mod clients;
 mod config;
 mod daemon;
@@ -20,7 +19,7 @@ pub fn run() {
     match config.command {
         Some(config::Command::Daemon) => daemon::run(config),
         Some(config::Command::Get) => clients::get::run(config),
-        Some(config::Command::Chat(..)) => chat_client::run(config),
+        Some(config::Command::Chat(..)) => clients::chat::run(config),
         Some(config::Command::Files) => file_client::run(config),
         None => daemon_client::run(config),
     }
