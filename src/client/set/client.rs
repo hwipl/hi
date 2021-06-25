b9551cfb46b8715e08ebaac7becced7098379ff7
hwipl <33433250+hwipl@users.noreply.github.com>
2021-06-25 11:10:38 +0200

    client/set: handle set options in config
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/client/set/client.rs
+++ src/client/set/client.rs
@@ -1,19 +1,56 @@
 use crate::config;
+use crate::daemon_message::Message;
 use crate::unix_socket;
 use async_std::task;
 
 /// set client
 struct SetClient {
-    _config: config::Config,
-    _client: unix_socket::UnixClient,
+    config: config::Config,
+    client: unix_socket::UnixClient,
 }
 
 impl SetClient {
-    async fn new(_config: config::Config, _client: unix_socket::UnixClient) -> Self {
-        SetClient { _config, _client }
+    /// create new set client
+    async fn new(config: config::Config, client: unix_socket::UnixClient) -> Self {
+        SetClient { config, client }
     }
 
-    async fn run(&self) {}
+    /// run set client
+    async fn run(&mut self) {
+        // get options to set from config
+        let options = match self.config.command {
+            Some(config::Command::Set(ref set_opts)) => &set_opts.opts,
+            _ => return,
+        };
+
+        // handle set configuration options
+        for option in options.iter() {
+            // create message
+            let msg = match option.name.as_str() {
+                "name" => Message::SetName {
+                    name: option.value.to_string(),
+                },
+                _ => {
+                    error!(
+                        "error setting unknown configuration option: {}",
+                        option.name
+                    );
+                    continue;
+                }
+            };
+
+            // send message
+            if let Err(e) = self.client.send_message(msg).await {
+                error!("error sending set message: {}", e);
+            }
+
+            // receive reply
+            match self.client.receive_message().await {
+                Ok(msg) => debug!("set reply from server: {:?}", msg),
+                Err(e) => error!("error receiving set reply: {}", e),
+            }
+        }
+    }
 }
 
 /// run set client
