b69609c214c0394564819929ea1be574e4b306a4
hwipl <33433250+hwipl@users.noreply.github.com>
2021-06-28 15:40:20 +0200

    client/get: register client on daemon
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/client/get/client.rs
+++ src/client/get/client.rs
@@ -2,25 +2,50 @@ use crate::config;
 use crate::daemon_message::Message;
 use crate::unix_socket;
 use async_std::task;
+use std::error::Error;
 
 /// get client
 struct GetClient {
     config: config::Config,
     client: unix_socket::UnixClient,
+    client_id: u16,
 }
 
 impl GetClient {
     /// create new get client
     async fn new(config: config::Config, client: unix_socket::UnixClient) -> Self {
-        GetClient { config, client }
+        GetClient {
+            config,
+            client,
+            client_id: 0,
+        }
+    }
+
+    /// register this client
+    async fn register_client(&mut self) -> Result<(), Box<dyn Error>> {
+        let msg = Message::Register {
+            chat: false,
+            files: false,
+        };
+        self.client.send_message(msg).await?;
+        match self.client.receive_message().await? {
+            Message::RegisterOk { client_id } => {
+                self.client_id = client_id;
+                Ok(())
+            }
+            _ => Err("unexpected message from daemon".into()),
+        }
     }
 
     /// run get client
-    async fn run(&mut self) {
+    async fn run(&mut self) -> Result<(), Box<dyn Error>> {
+        // register client
+        self.register_client().await?;
+
         // get options to get from config
         let options = match self.config.command {
             Some(config::Command::Get(ref get_opts)) => &get_opts.info,
-            _ => return,
+            _ => return Err("invalid config".into()),
         };
 
         // handle get configuration options
@@ -37,36 +62,28 @@ impl GetClient {
             };
 
             // send message
-            if let Err(e) = self.client.send_message(msg).await {
-                error!("error sending get message: {}", e);
-            }
+            self.client.send_message(msg).await?;
 
             // receive reply
-            match self.client.receive_message().await {
-                Ok(msg) => {
-                    debug!("get reply from server: {:?}", msg);
-                    match msg {
-                        Message::GetName { name } => println!("Name: {}", name),
-                        Message::GetPeers { peers } => {
-                            println!("Peers:");
-                            for peer in peers {
-                                println!(
-                                    "  peer_id: {}, name: {:?}, chat_support: {}, file_support: {}, last_update: {}",
-                                    peer.peer_id,
-                                    peer.name,
-                                    peer.chat_support,
-                                    peer.file_support,
-                                    peer.last_update
-                                );
-                            }
-                        }
-                        Message::Error { message } => println!("Error: {}", message),
-                        _ => println!("{:?}", msg),
+            match self.client.receive_message().await? {
+                Message::GetName { name } => println!("Name: {}", name),
+                Message::GetPeers { peers } => {
+                    println!("Peers:");
+                    for peer in peers {
+                        println!("  peer_id: {}, name: {:?}, chat_support: {}, file_support: {}, last_update: {}",
+                            peer.peer_id,
+                            peer.name,
+                            peer.chat_support,
+                            peer.file_support,
+                            peer.last_update
+                            );
                     }
                 }
-                Err(e) => error!("error receiving get reply: {}", e),
+                Message::Error { message } => println!("Error: {}", message),
+                msg => println!("{:?}", msg),
             }
         }
+        Ok(())
     }
 }
 
@@ -74,7 +91,11 @@ impl GetClient {
 pub fn run(config: config::Config) {
     task::block_on(async {
         match unix_socket::UnixClient::connect(&config).await {
-            Ok(client) => GetClient::new(config, client).await.run().await,
+            Ok(client) => {
+                if let Err(e) = GetClient::new(config, client).await.run().await {
+                    error!("{}", e);
+                }
+            }
             Err(e) => error!("unix socket client error: {}", e),
         }
         debug!("get client stopped");
