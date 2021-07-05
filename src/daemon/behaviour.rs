d5019c8324ca6eb15c448f565417a54e1867af42
hwipl <33433250+hwipl@users.noreply.github.com>
2021-07-05 11:46:44 +0200

    daemon/behaviour: add handle_request()
    Signed-off-by: hwipl <33433250+hwipl@users.noreply.github.com>

--- src/daemon/behaviour.rs
+++ src/daemon/behaviour.rs
@@ -8,7 +8,7 @@ use libp2p::gossipsub::{Gossipsub, GossipsubEvent};
 use libp2p::mdns::{Mdns, MdnsEvent};
 use libp2p::request_response::{RequestResponse, RequestResponseEvent, RequestResponseMessage};
 use libp2p::swarm::NetworkBehaviourEventProcess;
-use libp2p::NetworkBehaviour;
+use libp2p::{NetworkBehaviour, PeerId};
 
 /// Custom network behaviour with mdns, gossipsub, request-response
 #[derive(NetworkBehaviour)]
@@ -20,6 +20,62 @@ pub struct HiBehaviour {
     // channel for sending events to the swarm
     #[behaviour(ignore)]
     pub to_swarm: mpsc::UnboundedSender<swarm::Event>,
+}
+
+impl HiBehaviour {
+    /// handle request response "request" message
+    fn handle_request(&mut self, peer: PeerId, request: HiRequest) -> HiResponse {
+        match request {
+            // handle chat message
+            HiRequest::ChatMessage(msg) => {
+                debug!("received chat message: {}", msg);
+                let swarm_event = swarm::Event::ChatMessage(peer.to_base58(), msg);
+                let mut to_swarm = self.to_swarm.clone();
+                task::spawn(async move {
+                    if let Err(e) = to_swarm.send(swarm_event).await {
+                        error!("error sending event to swarm: {}", e);
+                    }
+                });
+                HiResponse::Ok
+            }
+
+            // handle chat message
+            HiRequest::FileMessage(to_client, from_client, content) => {
+                debug!("received file message: {:?}", content);
+                let swarm_event =
+                    swarm::Event::FileMessage(peer.to_base58(), from_client, to_client, content);
+                let mut to_swarm = self.to_swarm.clone();
+                task::spawn(async move {
+                    if let Err(e) = to_swarm.send(swarm_event).await {
+                        error!("error sending event to swarm: {}", e);
+                    }
+                });
+                HiResponse::Ok
+            }
+
+            // handle message
+            HiRequest::Message(to_client, from_client, service, content) => {
+                debug!("received message: {:?}", content);
+                let swarm_event = swarm::Event::Message(
+                    peer.to_base58(),
+                    from_client,
+                    to_client,
+                    service,
+                    content,
+                );
+                let mut to_swarm = self.to_swarm.clone();
+                task::spawn(async move {
+                    if let Err(e) = to_swarm.send(swarm_event).await {
+                        error!("error sending event to swarm: {}", e);
+                    }
+                });
+                HiResponse::Ok
+            }
+
+            // handle other requests
+            _ => HiResponse::Error(String::from("unknown request")),
+        }
+    }
 }
 
 impl NetworkBehaviourEventProcess<RequestResponseEvent<HiRequest, HiResponse>> for HiBehaviour {
@@ -38,60 +94,7 @@ impl NetworkBehaviourEventProcess<RequestResponseEvent<HiRequest, HiResponse>> f
                         "received request {:?} with id {} from {:?}",
                         request, request_id, peer
                     );
-                    let response = match request {
-                        // handle chat message
-                        HiRequest::ChatMessage(msg) => {
-                            debug!("received chat message: {}", msg);
-                            let swarm_event = swarm::Event::ChatMessage(peer.to_base58(), msg);
-                            let mut to_swarm = self.to_swarm.clone();
-                            task::spawn(async move {
-                                if let Err(e) = to_swarm.send(swarm_event).await {
-                                    error!("error sending event to swarm: {}", e);
-                                }
-                            });
-                            HiResponse::Ok
-                        }
-
-                        // handle chat message
-                        HiRequest::FileMessage(to_client, from_client, content) => {
-                            debug!("received file message: {:?}", content);
-                            let swarm_event = swarm::Event::FileMessage(
-                                peer.to_base58(),
-                                from_client,
-                                to_client,
-                                content,
-                            );
-                            let mut to_swarm = self.to_swarm.clone();
-                            task::spawn(async move {
-                                if let Err(e) = to_swarm.send(swarm_event).await {
-                                    error!("error sending event to swarm: {}", e);
-                                }
-                            });
-                            HiResponse::Ok
-                        }
-
-                        // handle message
-                        HiRequest::Message(to_client, from_client, service, content) => {
-                            debug!("received message: {:?}", content);
-                            let swarm_event = swarm::Event::Message(
-                                peer.to_base58(),
-                                from_client,
-                                to_client,
-                                service,
-                                content,
-                            );
-                            let mut to_swarm = self.to_swarm.clone();
-                            task::spawn(async move {
-                                if let Err(e) = to_swarm.send(swarm_event).await {
-                                    error!("error sending event to swarm: {}", e);
-                                }
-                            });
-                            HiResponse::Ok
-                        }
-
-                        // handle other requests
-                        _ => HiResponse::Error(String::from("unknown request")),
-                    };
+                    let response = self.handle_request(peer, request);
                     self.request.send_response(channel, response).unwrap();
                     return;
                 }
